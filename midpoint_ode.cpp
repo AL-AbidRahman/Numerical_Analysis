4e298a291dd75a1d0806bdc2c477283d3f099039
AL-AbidRahman <alabidrahman1019@gmail.com>
2025-10-29 04:05:18 +0600

    Add files via upload

---
+++ midpoint_ode.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double slope(double x, double y){
+
+double numerator = x*x + y*y;
+double denominator = x + y;
+
+return (numerator/denominator);
+
+}
+
+int main(){
+
+double y0 = 1; double x0 = 0;
+
+double step = 0.25;
+
+int size = (1.0-0.0)/(0.25);
+
+double arr[size+1];
+
+for(int i=1; i<=size; i++){
+
+arr[i] = y0 + slope( x0 + (step/2.0), y0 + slope(x0,y0)*(step/2.0) )*step; 
+
+x0 = x0 + step;
+y0 = arr[i];
+
+}
+
+for(int i=1; i<size+1; i++) cout<<arr[i]<<endl;
+
+
+  return 0;
+}
